f73e86c3fb12df6ff2084ada5db80b1f46f851cf
John Wiseman <jjwiseman@gmail.com>
2023-04-19 22:49:12 -0700

    Added docs, changed crate name.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,46 @@
+//! A library for finding the country of origin for an ICAO hex ID.
+//!
+//! # Example
+//!
+//! ```
+//! use aircraft_icao_country::Allocs;
+//!
+//! let allocs = Allocs::new();
+//! assert_eq!(allocs.find_from_hex("a67bd3"), Some("United States"));
+//! ```
+//!
+//! # References
+//!
+//! * [Registration Of Aircraft Addresses With Mode S Transponders, Appendix A](https://www.icao.int/Meetings/AMC/MA/NACC_DCA03_2008/naccdca3wp05.pdf)
+//!
+//! # License
+//!
+//! This library is licensed under the MIT license.
+
+/// A struct representing the allocation of ICAO hex IDs to countries.
 pub struct Allocs {
-    allocs: Vec<Range>
+    pub ranges: Vec<Range>,
 }
 
 impl Allocs {
+    /// Create a new `Allocs` instance by parsing the country ranges CSV file.
     pub fn new() -> Allocs {
         Allocs {
-            allocs: parse_range_csv(COUNTRY_RANGES_CSV),
+            ranges: parse_range_csv(COUNTRY_RANGES_CSV),
         }
     }
 
-    pub fn find(&self, ip: u32) -> Option<&str> {
-        for range in self.allocs.iter() {
-            if ip >= range.start && ip <= range.end {
+    /// Find the country for the given ICAO hex ID (as a `u32`).
+    pub fn find(&self, icao: u32) -> Option<&str> {
+        for range in self.ranges.iter() {
+            if icao >= range.start && icao <= range.end {
                 return Some(&range.country);
             }
         }
         None
     }
 
+    /// Find the country for the given ICAO hex ID (as a `&str`).
     pub fn find_from_hex(&self, hex: &str) -> Option<&str> {
         // Return None if the hex string is not a valid u32.
         let ip = u32::from_str_radix(hex, 16).ok()?;
@@ -25,18 +48,21 @@ impl Allocs {
     }
 }
 
+/// Implement the `Default` trait for `Allocs` struct.
 impl Default for Allocs {
     fn default() -> Self {
         Self::new()
     }
 }
 
+/// A struct representing a range of ICAO hex IDs allocated to a specific country.
 pub struct Range {
     pub start: u32,
     pub end: u32,
     pub country: String,
 }
 
+/// Parses a CSV string and return a `Vec<Range>`.
 pub fn parse_range_csv(input: &str) -> Vec<Range> {
     let mut ranges = Vec::new();
     let mut lines = input.lines();
@@ -66,6 +92,7 @@ mod tests {
     use super::*;
 
     #[test]
+    /// Test the `find_country_from_hex()` method for various cases.
     fn test_find_country_from_hex() {
         let allocs = Allocs::new();
         assert_eq!(allocs.find_from_hex("a67bd3"), Some("United States"));
